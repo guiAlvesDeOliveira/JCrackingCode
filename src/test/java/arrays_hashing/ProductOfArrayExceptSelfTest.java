c23d495230ca546f081c4c69c1c2a11699380206
J34993R <guialves.oliveira1234@gmail.com>
2023-10-26 10:20:01 -0300

    Add Leetcode problem 238 Product Of Array ExceptSelf
    Memory Usage: 52.64 MB
    Runtime: 1 ms

---
+++ src/test/java/arrays_hashing/ProductOfArrayExceptSelfTest.java
@@ -0,0 +1,23 @@
+package arrays_hashing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ProductOfArrayExceptSelfTest {
+
+    @Test
+    void test1() {
+        ProductOfArrayExceptSelf validator = new ProductOfArrayExceptSelf();
+        int[] nums = new int[]{1, 2, 3, 4};
+        int[] expected = new int[]{24, 12, 8, 6};
+        assertArrayEquals(expected, validator.productExceptSelf(nums));
+    }
+    @Test
+    void test2() {
+        ProductOfArrayExceptSelf validator = new ProductOfArrayExceptSelf();
+        int[] nums = new int[]{-1,1,0,-3,3};
+        int[] expected = new int[]{0,0,9,0,0};
+        assertArrayEquals(expected, validator.productExceptSelf(nums));
+    }
+}
